e8ba01c44a5628e26032ac8850a15c3b06f5c034
sulox32 <s-coskun32@hotmail.de>
2018-01-16 13:15:49 +0100

    ...

---
+++ Assets/MainSample/Scripts/Leap/EmergencyButtonGraspController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmergencyButtonGraspController : BaseGraspController {
+
+    public float maxOpeningDegree = 90;
+
+
+    private HingeJoint hinge;
+    private JointSpring spring;
+
+    public override void cancelGraspAction() {
+        //Evtl zuruecksetzen
+    }
+
+
+    public override void doGraspAction() {
+        spring.targetPosition = 15; //Irgend eine Berechnung, wenn das Ding gegriffen wird
+        //Nicht über 90 grad
+
+        hinge.spring = spring;
+    }
+
+    public override void init() {
+        hinge = this.GetComponentInChildren<HingeJoint>();
+        spring = hinge.spring;
+    }
+}
